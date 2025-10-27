676ac780b5dc72dc125e1245c04e7274e56b17a6
Nik Sumeiko <nik.sumeiko@gmail.com>
2025-10-27 13:20:29 +0100

    test: Fake correct answer

--- src/__tests__/ResolveRiddle.test.tsx
+++ src/__tests__/ResolveRiddle.test.tsx
@@ -1,9 +1,18 @@
 import { App } from '../App';
+import { ContextProvider } from '../common/context';
+import { provideRiddleAnswer } from '../domain/riddle/RiddleAnswerProvider';
 
 describe('ResolveRiddle Test', () => {
     beforeEach(() => {
-        cy.mount(<App />, '/riddle/RIDDLE_ID');
-
+        cy.intercept('GET', '**/riddles', {
+            body: [
+                {
+                    id: 'RIDDLE_ID',
+                    contents: 'Riddle contents',
+                    answers: [{ id: 'ANSWER_ID', text: 'Answer 1' }]
+                }
+            ],
+        });
         cy.intercept('GET', '**/riddles/RIDDLE_ID', {body: {
                 "id": "RIDDLE_ID",
                 "contents": "At my post, I watch them dance, moving dots both near and far, but one stands still and claims to cross the path of all who dare to pass.<br /><br />What is it?",
@@ -26,7 +35,17 @@ describe('ResolveRiddle Test', () => {
     })
 
     it('resolve riddle correctly', () => {
+        const fake = () => Promise.resolve({
+            id: '1',
+            text: 'A holding pattern',
+        });
 
+        cy.mount(
+            <ContextProvider providers={[provideRiddleAnswer(fake)]}>
+                <App />
+            </ContextProvider>,
+            '/riddle/RIDDLE_ID',
+        );
 
         cy.getByTestId('riddle-option-1').click()
 
